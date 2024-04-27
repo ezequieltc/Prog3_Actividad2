bb929ecb457d31c812c1624782e8a67bf20aa0ae
Ezequiel Teruel <ezequielteruel@hotmail.com>
2024-04-27 17:16:47 -0300

    Se agrega el menu Categoria.

---
+++ Programacion 3/MenuCategoria.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Programacion_3
+{
+    partial class MenuCategoria
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonDescartar = new System.Windows.Forms.Button();
+            this.buttonGuardar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxDescripcion = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dgvCategoria = new System.Windows.Forms.DataGridView();
+            this.buttonAgregar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxAgregar = new System.Windows.Forms.TextBox();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.checkBoxEdicion = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCategoria)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonDescartar
+            // 
+            this.buttonDescartar.Location = new System.Drawing.Point(262, 403);
+            this.buttonDescartar.Name = "buttonDescartar";
+            this.buttonDescartar.Size = new System.Drawing.Size(88, 27);
+            this.buttonDescartar.TabIndex = 15;
+            this.buttonDescartar.Text = "Descartar";
+            this.buttonDescartar.UseVisualStyleBackColor = true;
+            // 
+            // buttonGuardar
+            // 
+            this.buttonGuardar.Location = new System.Drawing.Point(28, 403);
+            this.buttonGuardar.Name = "buttonGuardar";
+            this.buttonGuardar.Size = new System.Drawing.Size(88, 27);
+            this.buttonGuardar.TabIndex = 14;
+            this.buttonGuardar.Text = "Guardar";
+            this.buttonGuardar.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 333);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Descripcion:";
+            // 
+            // textBoxDescripcion
+            // 
+            this.textBoxDescripcion.Enabled = false;
+            this.textBoxDescripcion.Location = new System.Drawing.Point(12, 352);
+            this.textBoxDescripcion.Name = "textBoxDescripcion";
+            this.textBoxDescripcion.Size = new System.Drawing.Size(358, 20);
+            this.textBoxDescripcion.TabIndex = 12;
+            this.textBoxDescripcion.DoubleClick += new System.EventHandler(this.DobleClickDescripcion);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(7, 270);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 25);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Detalle:";
+            // 
+            // dgvCategoria
+            // 
+            this.dgvCategoria.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCategoria.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dgvCategoria.Location = new System.Drawing.Point(12, 93);
+            this.dgvCategoria.MultiSelect = false;
+            this.dgvCategoria.Name = "dgvCategoria";
+            this.dgvCategoria.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvCategoria.Size = new System.Drawing.Size(358, 150);
+            this.dgvCategoria.TabIndex = 10;
+            this.dgvCategoria.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.ClickEnCelda);
+            // 
+            // buttonAgregar
+            // 
+            this.buttonAgregar.Location = new System.Drawing.Point(6, 47);
+            this.buttonAgregar.Name = "buttonAgregar";
+            this.buttonAgregar.Size = new System.Drawing.Size(88, 27);
+            this.buttonAgregar.TabIndex = 9;
+            this.buttonAgregar.Text = "Agregar";
+            this.buttonAgregar.UseVisualStyleBackColor = true;
+            this.buttonAgregar.Click += new System.EventHandler(this.buttonAgregar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 25);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Categoria:";
+            // 
+            // textBoxAgregar
+            // 
+            this.textBoxAgregar.Location = new System.Drawing.Point(100, 51);
+            this.textBoxAgregar.Name = "textBoxAgregar";
+            this.textBoxAgregar.Size = new System.Drawing.Size(269, 20);
+            this.textBoxAgregar.TabIndex = 16;
+            this.toolTip.SetToolTip(this.textBoxAgregar, "Indique el nombre de la categoria a agregar");
+            // 
+            // toolTip
+            // 
+            this.toolTip.AutoPopDelay = 5000;
+            this.toolTip.BackColor = System.Drawing.Color.Bisque;
+            this.toolTip.InitialDelay = 250;
+            this.toolTip.ReshowDelay = 100;
+            // 
+            // checkBoxEdicion
+            // 
+            this.checkBoxEdicion.AutoSize = true;
+            this.checkBoxEdicion.Location = new System.Drawing.Point(12, 299);
+            this.checkBoxEdicion.Name = "checkBoxEdicion";
+            this.checkBoxEdicion.Size = new System.Drawing.Size(91, 17);
+            this.checkBoxEdicion.TabIndex = 17;
+            this.checkBoxEdicion.Text = "Modo Edicion";
+            this.checkBoxEdicion.UseVisualStyleBackColor = true;
+            this.checkBoxEdicion.CheckedChanged += new System.EventHandler(this.checkBoxEdicion_CheckedChanged);
+            // 
+            // MenuCategoria
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 450);
+            this.Controls.Add(this.checkBoxEdicion);
+            this.Controls.Add(this.textBoxAgregar);
+            this.Controls.Add(this.buttonDescartar);
+            this.Controls.Add(this.buttonGuardar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxDescripcion);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dgvCategoria);
+            this.Controls.Add(this.buttonAgregar);
+            this.Controls.Add(this.label1);
+            this.Name = "MenuCategoria";
+            this.Text = "MenuCategoria";
+            this.Load += new System.EventHandler(this.MenuCategoria_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCategoria)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonDescartar;
+        private System.Windows.Forms.Button buttonGuardar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxDescripcion;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dgvCategoria;
+        private System.Windows.Forms.Button buttonAgregar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxAgregar;
+        private System.Windows.Forms.ToolTip toolTip;
+        private System.Windows.Forms.CheckBox checkBoxEdicion;
+    }
+}
---
+++ Programacion 3/MenuCategoria.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using dominio;
+using negocio;
+
+namespace Programacion_3
+{
+    public partial class MenuCategoria : Form
+    {
+        public MenuCategoria()
+        {
+            InitializeComponent();
+        }
+        private void MenuCategoria_Load(object sender, EventArgs e)
+        {
+            CategoriaNegocio categoriaNegocio = new CategoriaNegocio();
+            dgvCategoria.DataSource = categoriaNegocio.listarCategorias();
+            dgvCategoria.Rows[0].Selected = true;
+            Categoria seleccion = (Categoria)dgvCategoria.CurrentRow.DataBoundItem;
+            textBoxDescripcion.Text = seleccion.ToString();
+        }
+        private void ClickEnCelda(object sender, DataGridViewCellEventArgs e)
+        {
+            Categoria seleccion = (Categoria)dgvCategoria.CurrentRow.DataBoundItem;
+            textBoxDescripcion.Text = seleccion.ToString();
+        }
+
+        private void DobleClickDescripcion(object sender, EventArgs e)
+        {
+            textBoxDescripcion.ReadOnly = false;
+        }
+
+        private void buttonAgregar_Click(object sender, EventArgs e)
+        {
+            CategoriaNegocio categoriaNegocio = new CategoriaNegocio();
+            if(textBoxAgregar.Text.Length > 0 && !Regex.IsMatch(textBoxAgregar.Text, @"^\s"))
+            {
+                try
+                {
+                    categoriaNegocio.agregar(textBoxAgregar.Text);
+                    textBoxAgregar.Text = "";
+                    dgvCategoria.DataSource = categoriaNegocio.listarCategorias();
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show("Error " + ex.ToString());
+                }
+            }
+            else
+            {
+                MessageBox.Show("La descripción no puede estar en blanco ni empezar con espacios.");
+            }
+            
+        }
+
+        private void checkBoxEdicion_CheckedChanged(object sender, EventArgs e)
+        {
+            textBoxDescripcion.Enabled = checkBoxEdicion.Checked;
+            
+        }
+    }
+}
+
